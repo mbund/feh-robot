57b534652a736be449c05f527b07a97dcc7facc8
Mark Bundschuh <mark@mbund.org>
2023-02-07 11:53:46 -0500

    update attribution

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+/// @file main.cpp
+/// @author Mark Bundschuh
+/// @brief Contains the entrypoint for the program
+
 #include <FEHAccel.h>
 #include <FEHBattery.h>
 #include <FEHBuzzer.h>
@@ -11,6 +15,7 @@
 #include <cstdio>
 #include <cstring>
 
+/// Main function which is the entrypoint for the entire program
 auto main() -> int {
     LCD.Clear(WHITE);
 }
