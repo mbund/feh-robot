fb19b802ee162e3f5f9e4b2cabae52087ade9fe7
Mark Bundschuh <mark@mbund.org>
2023-02-13 17:48:04 -0500

    begin ui

--- main.cpp
+++ main.cpp
@@ -5,15 +5,20 @@
 #include <FEHLCD.h>
 #include <FEHMotor.h>
 #include <FEHUtility.h>
+#include <LCDColors.h>
 
 #include <algorithm>
 #include <cstdlib>
+#include <functional>
 #include <memory>
+#include <string>
 #include <vector>
 
 const auto LCD_WIDTH = 320;
 const auto LCD_HEIGHT = 240;
-int touchX, touchY;
+const auto FONT_WIDTH = 12;
+const auto FONT_HEIGHT = 17;
+float touchX, touchY;
 bool touchPressed;
 
 /// Wrapper for the FEHMotor class
@@ -24,29 +29,40 @@ class Motor {
 
     /// Sets the power of the motor
     /// @param power The power to set the motor to between -1 and 1
-    auto drive(double power) const -> void;
+    auto drive(double power) -> void;
 
    private:
     FEHMotor motor;
     double correction_factor;
 };
 
+Motor::Motor(FEHMotor::FEHMotorPort port, double correction_factor)
+    : motor(port, 9.0), correction_factor(correction_factor) {}
+
+auto Motor::drive(double power) -> void {
+    auto percent = std::clamp(power * correction_factor, -1.0, 1.0);
+    motor.SetPercent(percent * 100.0);
+}
+
 /// Base class for all steps
 class Step {
    public:
     /// Constructor for the Step class
-    Step();
+    Step(std::string name);
 
     /// Executes the step
     /// @param t The current time
     /// @return Whether the step is done
     virtual auto execute(double t) -> bool;
 
+    /// The name of the step
+    std::string name;
+
     /// The time at which the step starts
     double t_start;
 };
 
-Step::Step() : t_start(-1) {}
+Step::Step(std::string name) : t_start(-1), name(name) {}
 
 auto Step::execute(double t) -> bool {
     if (t_start < 0)
@@ -59,7 +75,10 @@ auto Step::execute(double t) -> bool {
 class TranslateStep : public Step {
    public:
     /// Translate (move) the robot for a given duration at a given heading
-    TranslateStep(double duration, double heading);
+    /// @param name The name of the step
+    /// @param duration The duration of the translation
+    /// @param heading The heading (angle) to translate towards
+    TranslateStep(std::string name, double duration, double heading);
 
     /// Execute the translation step
     auto execute(double t) -> bool override;
@@ -69,11 +88,12 @@ class TranslateStep : public Step {
     double duration;
 };
 
-TranslateStep::TranslateStep(double duration, double heading)
-    : Step(), duration(duration), heading(heading) {}
+TranslateStep::TranslateStep(std::string name, double duration, double heading)
+    : Step(name), duration(duration), heading(heading) {}
 
 auto TranslateStep::execute(double t) -> bool {
     Step::execute(t);
+    LCD.SetFontColor(WHITE);
     LCD.WriteAt("Translate step", 0, 0);
 
     return t >= t_start + duration;
@@ -89,9 +109,10 @@ class EndStep : public Step {
     auto execute(double t) -> bool override;
 };
 
-EndStep::EndStep() : Step() {}
+EndStep::EndStep() : Step("End") {}
 
 auto EndStep::execute(double t) -> bool {
+    LCD.SetFontColor(WHITE);
     LCD.WriteAt("End step", 0, 0);
 
     return true;
@@ -101,9 +122,10 @@ auto EndStep::execute(double t) -> bool {
 class RotateStep : public Step {
    public:
     /// Rotate the robot for a given duration by a given angle
+    /// @param name The name of the step
     /// @param duration The duration of the rotation
     /// @param theta The angle to rotate by
-    RotateStep(double duration, double theta);
+    RotateStep(std::string name, double duration, double theta);
 
     /// Execute the rotation step
     auto execute(double t) -> bool override;
@@ -113,11 +135,12 @@ class RotateStep : public Step {
     double duration;
 };
 
-RotateStep::RotateStep(double duration, double theta)
-    : Step(), duration(duration), theta(theta) {}
+RotateStep::RotateStep(std::string name, double duration, double theta)
+    : Step(name), duration(duration), theta(theta) {}
 
 auto RotateStep::execute(double t) -> bool {
     Step::execute(t);
+    LCD.SetFontColor(WHITE);
     LCD.WriteAt("Rotate step", 0, 0);
 
     return t >= t_start + duration;
@@ -140,9 +163,10 @@ std::vector<std::shared_ptr<Base>> make_vector_of_shared(Ts&&... ts) {
 class UnionStep : public Step {
    public:
     /// Constructor for a union step
+    /// @param name The name of the step
     /// @param ts The steps to execute in parallel
     template <typename... Ts>
-    UnionStep(Ts&&... ts);
+    UnionStep(std::string name, Ts&&... ts);
 
     /// Execute the union step
     auto execute(double t) -> bool override;
@@ -152,8 +176,8 @@ class UnionStep : public Step {
 };
 
 template <typename... Ts>
-UnionStep::UnionStep(Ts&&... ts)
-    : Step(), steps(make_vector_of_shared<Step>(std::forward<Ts>(ts)...)) {}
+UnionStep::UnionStep(std::string name, Ts&&... ts)
+    : Step(name), steps(make_vector_of_shared<Step>(std::forward<Ts>(ts)...)) {}
 
 auto UnionStep::execute(double t) -> bool {
     Step::execute(t);
@@ -168,6 +192,91 @@ auto UnionStep::execute(double t) -> bool {
     return steps.size() == 0;
 }
 
+/// Represents a rectangle
+struct Rect {
+    /// Constructor for a rectangle
+    /// @param x The x coordinate of the top left corner
+    /// @param y The y coordinate of the top left corner
+    /// @param width The width of the rectangle
+    /// @param height The height of the rectangle
+    Rect(unsigned int x,
+         unsigned int y,
+         unsigned int width,
+         unsigned int height)
+        : x(x), y(y), width(width), height(height) {}
+
+    /// The x coordinate of the top left corner
+    unsigned int x;
+
+    /// The y coordinate of the top left corner
+    unsigned int y;
+
+    /// The width of the rectangle
+    unsigned int width;
+
+    /// The height of the rectangle
+    unsigned int height;
+};
+
+class TouchableRegion {
+   public:
+    /// Constructor for a touchable region
+    /// @param rect The rectangle that represents the region
+    /// @param on_button_enter The function to call when the button is pressed
+    /// @param on_button_exit The function to call when the button is released
+    TouchableRegion(
+        Rect rect,
+        std::function<void()> on_button_enter = []() {},
+        std::function<void()> on_button_exit = []() {});
+
+    /// Checks the touch sensor and updates the state of the button
+    auto update() -> void;
+
+    /// The rectangle that represents the region
+    Rect rect;
+
+   private:
+    /// Type to represent the state of the button
+    typedef enum {
+        InBounds,
+        OutOfBounds,
+    } ButtonState;
+
+    /// The current state of the button
+    ButtonState state;
+
+    /// The function to call when the button is pressed
+    std::function<void()> on_button_enter;
+
+    /// The function to call when the button is released
+    std::function<void()> on_button_exit;
+};
+
+TouchableRegion::TouchableRegion(Rect rect,
+                                 std::function<void()> on_button_enter,
+                                 std::function<void()> on_button_exit)
+    : rect(rect),
+      state(ButtonState::OutOfBounds),
+      on_button_enter(on_button_enter),
+      on_button_exit(on_button_exit) {}
+
+auto TouchableRegion::update() -> void {
+    float y_offset = -4;  // our Proteus innacurately reports touch location, so
+                          // we must account for it
+
+    bool in_bounds = touchX >= rect.x && touchX <= rect.x + rect.width &&
+                     touchY + y_offset >= rect.y &&
+                     touchY + y_offset <= rect.y + rect.height;
+
+    if (in_bounds && touchPressed && state == ButtonState::OutOfBounds) {
+        state = ButtonState::InBounds;
+        on_button_enter();
+    } else if (!in_bounds && state != ButtonState::OutOfBounds) {
+        state = ButtonState::OutOfBounds;
+        on_button_exit();
+    }
+}
+
 /// Sequence of steps to execute
 class Timeline {
    public:
@@ -178,16 +287,56 @@ class Timeline {
 
     /// Execute the timeline
     /// @param t The current time
+    /// @return True if the timeline is finished executing
     auto timestep(double t) -> bool;
 
+    /// Update the timeline UI
+    auto update() -> void;
+
    private:
+    /// The steps to execute
     const std::vector<std::shared_ptr<Step>> steps;
+
+    /// The UI regions which can be touched
+    std::vector<TouchableRegion> regions;
+
+    /// The index of the current step
     size_t current_step_index = 0;
 };
 
 template <typename... Ts>
 Timeline::Timeline(Ts&&... ts)
-    : steps(make_vector_of_shared<Step>(std::forward<Ts>(ts)...)) {}
+    : steps(make_vector_of_shared<Step>(std::forward<Ts>(ts)...)) {
+    Rect rect(10, 30, 250, FONT_HEIGHT + 2);
+    regions.reserve(steps.size());
+
+    for (const auto& step : steps) {
+        LCD.SetFontColor(BLUE);
+        LCD.DrawRectangle(rect.x, rect.y, rect.width, rect.height);
+        LCD.WriteAt(step->name.c_str(), rect.x + 1, rect.y + 2);
+        regions.push_back(TouchableRegion(
+            rect,
+            [=]() {
+                LCD.SetFontColor(GREEN);
+                LCD.DrawRectangle(rect.x, rect.y, rect.width, rect.height);
+            },
+            [=]() {
+                LCD.SetFontColor(BLUE);
+                LCD.DrawRectangle(rect.x, rect.y, rect.width, rect.height);
+            }));
+
+        // draw button controls
+        const auto MEASURE = rect.height;
+        LCD.DrawRectangle(
+            rect.x + rect.width - MEASURE * 1, rect.y, MEASURE, MEASURE);
+        LCD.DrawRectangle(
+            rect.x + rect.width - MEASURE * 2, rect.y, MEASURE, MEASURE);
+        LCD.DrawRectangle(
+            rect.x + rect.width - MEASURE * 3, rect.y, MEASURE, MEASURE);
+
+        rect.y += rect.height + 3;
+    }
+}
 
 auto Timeline::timestep(double t) -> bool {
     if (current_step_index >= steps.size())
@@ -200,17 +349,26 @@ auto Timeline::timestep(double t) -> bool {
     return true;
 }
 
+auto Timeline::update() -> void {
+    for (auto& region : regions)
+        region.update();
+}
+
 /// Main function which is the entrypoint for the entire program
 auto main() -> int {
+    LCD.Clear(BLACK);
+    LCD.SetFontColor(WHITE);
+
     Timeline timeline{
-        TranslateStep(1, 90),
-        UnionStep(RotateStep(1, 90), TranslateStep(3, 90)),
-        RotateStep(1, 90),
+        TranslateStep("Initial move", 1, 90),
+        UnionStep("Union",
+                  RotateStep("Second rotate", 1, 90),
+                  TranslateStep("Second move", 3, 90)),
+        RotateStep("Last rotate", 1, 90),
         EndStep(),
     };
 
     // Main loop
-
     auto t = 0.0;
     auto start_time = TimeNow();
 
@@ -218,6 +376,10 @@ auto main() -> int {
     while (running) {
         t = TimeNow() - start_time;
         touchPressed = LCD.Touch(&touchX, &touchY);
-        running = timeline.timestep(t);
+
+        // running = timeline.timestep(t);
+        timeline.timestep(t);
+
+        timeline.update();
     }
 }
